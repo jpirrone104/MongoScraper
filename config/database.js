218fc92563c66ab2adf115b29b8065e28aef2c1b
Jen Pirrone <pirronej104@gmail.com>
2019-03-28 09:10:53 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  database: mongodb://heroku_c1l0nv1n:mushd27drubfokdffm1jtp5pbf@ds159978.mlab.com:59978/heroku_c1l0nv1n || 'mongodb://localhost:27017/VegNews10'
+  database: 'mongodb://heroku_c1l0nv1n:mushd27drubfokdffm1jtp5pbf@ds159978.mlab.com:59978/heroku_c1l0nv1n' || 'mongodb://localhost:27017/VegNews10'
 };
